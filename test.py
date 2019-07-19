616fd24c9e2e989405ca45e40762f26f25baa40f
霍飞 <huofei@huofeideMacBook-Pro.local>
2019-07-19 19:57:11 +0800

    review heap sort

--- test.py
+++ test.py
@@ -1,27 +1,23 @@
-
-def merge_sort(arr):
+def heap_sort(arr):
     for i in range((len(arr)-1)//2,-1,-1):
         shift_down(arr,len(arr),i)
-    for i in range(len(arr)):
-        arr[0],arr[-1]=arr[-1],arr[0]
+    i=len(arr)-1
+    while(i>0):
+        arr[i],arr[0]=arr[0],arr[i]
         shift_down(arr,i,0)
-
-def shift_down(arr,n,k):
-    #只要有左孩子，就要进行比较
-    while 2*k+1<n:
-        j=2*k+1
+        i-=1
+def shift_down(arr,n,i):
+    while(2*i+1<n):
+        j=2*i+1
         if j+1<n and arr[j+1]>arr[j]:
-            j = j+1
-        if arr[k]>arr[j]:
+            j+=1
+        if arr[i]>arr[j]:
             break
-        arr[k],arr[j]=arr[j],arr[k]
-        k = j
+        arr[i],arr[j]=arr[j],arr[i]
+        i=j
+if __name__ == "__main__":
+    arr=[5,3,2,1,3,5,3,1]
+    heap_sort(arr)
+    print(arr,file=open("./res.txt","w"))
 
-arr=[5,4,3,2,1,111]
-merge_sort(arr)
-print(arr)
-'''
-n:需要恢复堆状态的数组元素个数
-k:要shift——down的元素位置
-'''
 
